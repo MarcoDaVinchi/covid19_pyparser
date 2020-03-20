d0d6f3ba694fcf634ffa6bcd032dfc44b12cce73
Магулаев Марат Тимурович <MagulaevMT@nevz.com>
2020-03-20 17:35:43 +0300

    implemented selenium scraper

--- index.py
+++ index.py
@@ -1,17 +1,50 @@
-from bs4 import BeautifulSoup
-import requests
+import json
+from pathlib import Path
+from time import sleep
+from os import getenv
+
 import html5lib
 import lxml
+import requests
+from bs4 import BeautifulSoup
+from dotenv import load_dotenv
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+
+basedir = Path.cwd()
+dotenv_path = basedir.joinpath(".env")
+
+if Path.exists(dotenv_path):
+    load_dotenv(dotenv_path)
+
+webdrivers_path = getenv("WEB_DRIVER")
+
+webdriver_chrome_path = basedir.joinpath(webdrivers_path).resolve()
+options = webdriver.ChromeOptions()
+options.add_argument("headless")
 
 url = "https://multimedia.scmp.com/widgets/china/wuhanvirus/"
-url1 = "https://interactive-static.scmp.com/sheet/wuhan/viruscases.json"
+viruscases_url = "https://interactive-static.scmp.com/sheet/wuhan/viruscases.json"
 
-page = requests.get(url)
-viruscases = requests.get(url1)
+driver = webdriver.Chrome(executable_path=webdriver_chrome_path, options=options)
+driver.get(url)
+sleep(10)
+# WebDriverWait(driver, timeout=10).until(lambda d: d.find_element_by_id("lastupdated"))
 
-print(page.status_code)
-print(viruscases.status_code)
+page = driver.page_source
+driver.quit()
 
-soup = BeautifulSoup(page.text, "html5lib")
+viruscases = requests.get(viruscases_url).json()
+
+# print(page)
+print('[LOG]Request status: %s' % requests.get(viruscases_url).status_code)
+# print(viruscases)
+
+with open(basedir.joinpath("viruscases.json"), "w") as outfile:
+    json.dump(viruscases, outfile, indent=4)
+
+soup = BeautifulSoup(page, "lxml")
 
 # print(soup)
+
+# total_cases = soup.find("div", class_="box cases")
