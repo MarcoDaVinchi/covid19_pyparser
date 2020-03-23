df12e1e7abf45a5b2e9936832fcdf1b59d134c07
Магулаев Марат Тимурович <MagulaevMT@nevz.com>
2020-03-23 16:33:42 +0300

    dump files

--- index.py
+++ index.py
@@ -1,18 +1,20 @@
 import json
+from os import getenv
 from pathlib import Path
 from time import sleep
-from os import getenv
 
-import html5lib
 import lxml
 import requests
 from bs4 import BeautifulSoup
 from dotenv import load_dotenv
 from selenium import webdriver
-from selenium.webdriver.support.ui import WebDriverWait
 
 basedir = Path.cwd()
 dotenv_path = basedir.joinpath(".env")
+dump_path = basedir.joinpath("dumps/")
+
+if not Path.exists(dump_path):
+    dump_path.mkdir(exist_ok=True)
 
 if Path.exists(dotenv_path):
     load_dotenv(dotenv_path)
@@ -28,23 +30,37 @@ viruscases_url = "https://interactive-static.scmp.com/sheet/wuhan/viruscases.jso
 
 driver = webdriver.Chrome(executable_path=webdriver_chrome_path, options=options)
 driver.get(url)
-sleep(10)
-# WebDriverWait(driver, timeout=10).until(lambda d: d.find_element_by_id("lastupdated"))
+sleep(15)
 
 page = driver.page_source
 driver.quit()
 
-viruscases = requests.get(viruscases_url).json()
+requested_cases = requests.get(viruscases_url)
+viruscases = requested_cases.json()
+entries = viruscases["entries"]
 
-# print(page)
-print('[LOG]Request status: %s' % requests.get(viruscases_url).status_code)
-# print(viruscases)
+total_cases = 0
+total_deceased = 0
+total_recovered = 0
 
-with open(basedir.joinpath("viruscases.json"), "w") as outfile:
+for entry in entries:
+    total_cases += int(entry["cases"].replace(",", ""))
+    total_deceased += int(entry["deaths"].replace(",", ""))
+    total_recovered += int(entry["recovered"].replace(",", ""))
+
+total = total_cases, total_deceased, total_recovered
+viruscases["summary"] = {
+    "total cases": total_cases,
+    "total deceased": total_deceased,
+    "total recovered": total_recovered,
+}
+
+print("Total cases: {}, Totally deceased: {}, Totally recovered: {}".format(*total))
+print("[LOG] Request status: %s" % requested_cases.status_code)
+
+with open(dump_path.joinpath("viruscases.json"), "w") as outfile:
     json.dump(viruscases, outfile, indent=4)
 
 soup = BeautifulSoup(page, "lxml")
-
-# print(soup)
-
-# total_cases = soup.find("div", class_="box cases")
+with open(dump_path.joinpath("wuhanvirus.html"), "w", encoding="utf-8") as outfile:
+    outfile.write(str(soup))
